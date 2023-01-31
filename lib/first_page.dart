75479fd4f73d10d2300c2aa84350b35a1397346c
Stefan Vlad <72700839+StefanVlad0@users.noreply.github.com>
2023-01-31 19:07:17 +0200

    login & signin pages

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -16,29 +16,36 @@ class _FirstPageState extends State<FirstPage> {
         backgroundColor: const Color(0xFF000534),
         body: SafeArea(
           child: Container(
+            ///gradientul de sus
             decoration: BoxDecoration(
-                gradient: RadialGradient(
-              center: Alignment.topCenter,
-              colors: [
-                Color.fromRGBO(50, 15, 98, 0.8),
-                Color(0xFF000534),
-              ],
-              stops: [
-                0.5,
-                1,
-              ],
-            )),
+              gradient: RadialGradient(
+                center: Alignment.topCenter,
+                colors: [
+                  Color.fromRGBO(50, 15, 98, 0.8),
+                  Color(0xFF000534),
+                ],
+                stops: [
+                  0.5,
+                  1,
+                ],
+              ),
+            ),
+
             child: Center(
               child: Column(
                 children: [
+                  ///Spatiu liber pana la poza
                   const SizedBox(
                     height: 60,
                   ),
+
+                  ///Logo
                   Image.asset(
                     'assets/images/Logo (1).png',
                     scale: 0.75,
                   ),
 
+                  ///Text "Service Finder"
                   Text("Service Finder",
                       style: GoogleFonts.outfit(
                         fontWeight: FontWeight.bold,
@@ -50,6 +57,8 @@ class _FirstPageState extends State<FirstPage> {
                   const SizedBox(
                     height: 115,
                   ),
+
+                  ///Buton Login
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 75),
                     child: Container(
@@ -70,9 +79,13 @@ class _FirstPageState extends State<FirstPage> {
                       ),
                     ),
                   ),
+
+                  ///Spatiu dintre buton login si "Don't have an account?"
                   const SizedBox(
                     height: 38,
                   ),
+
+                  ///Text "Don't have an account?"
                   Text("Don’t have an account?",
                       style: GoogleFonts.roboto(
                         fontWeight: FontWeight.bold,
@@ -83,6 +96,8 @@ class _FirstPageState extends State<FirstPage> {
                   const SizedBox(
                     height: 10,
                   ),
+
+                  ///Buton Sign In
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 75),
                     child: Container(
---
+++ lib/login_page.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(0xFF000534),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                ///Distanta dintre inceputul ecranului si Email address
+                const SizedBox(
+                  height: 130,
+                ),
+
+                ///Email adress
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        //<-- SEE HERE
+                        borderSide: BorderSide(
+                            width: 2,
+                            color: Color.fromRGBO(255, 255, 255, 0.4)),
+                      ),
+                      hintText: 'Email address',
+                      hintStyle: TextStyle(
+                        color: Color.fromRGBO(255, 255, 255, 0.4),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+                ///Password
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: TextField(
+                    obscureText: true,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        //<-- SEE HERE
+                        borderSide: BorderSide(
+                            width: 3,
+                            color: Color.fromRGBO(255, 255, 255, 0.4)),
+                      ),
+                      hintText: 'Password',
+                      hintStyle: TextStyle(
+                        color: Color.fromRGBO(255, 255, 255, 0.4),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+                ///Distanta dintre Password si Login
+                const SizedBox(
+                  height: 75,
+                ),
+
+                ///Buton Login
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(99, 26, 144, 0.5),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Login',
+                        style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 26,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                ///Spatiu dintre buton login si "Don't have an account?"
+                const SizedBox(
+                  height: 60,
+                ),
+
+                ///Text "Don't have an account?"
+                Text("Don’t have an account?",
+                    style: GoogleFonts.roboto(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(255, 255, 255, 0.4),
+                      fontSize: 16,
+                      letterSpacing: 1,
+                      decoration: TextDecoration.underline,
+                    )),
+
+                ///Distanta dintre Dont't have an account si Logo
+                const SizedBox(
+                  height: 50,
+                ),
+
+                ///Logo
+                Image.asset(
+                  'assets/images/Logo (1).png',
+                  scale: 1,
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
